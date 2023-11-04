3e7b2520dbcb7c81219742c80b70ade9a1b35653
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-11-04 13:47:03 +0100

    Update cluster.tf

--- terraform-kind-cluster/cluster.tf
+++ terraform-kind-cluster/cluster.tf
@@ -2,5 +2,5 @@ resource "kind_cluster" "default" {
     name = "demo-cluster"
     wait_for_ready = "true"
     kubeconfig = "true"
-    # node_image = "kindest/node:v1.16.1"
+    node_image = "kindest/node:v1.23.17"
 }
